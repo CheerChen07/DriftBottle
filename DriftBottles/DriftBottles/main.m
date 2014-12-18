fb03f042fdc6ef77e9f1218e2269b0f0214866e6
CheerChen07 <chqny@sina.com>
2014-12-18 12:12:53 +0800

    Revert "Revert "演示项目""
    This reverts commit 731a9d3cf7d52a34c26bcd73b3aacc03ad3f968b.

---
+++ DriftBottles/DriftBottles/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DriftBottles
+//
+//  Created by CheerChen on 14-12-16.
+//  Copyright (c) 2014年 ustcsse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
