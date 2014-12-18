6e2ab2b13b1285d2a106cf98acf1e4b231f6b62c
CheerChen07 <chqny@sina.com>
2014-12-18 12:08:06 +0800

    演示项目

---
+++ DriftBottles/DriftBottles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DriftBottles
+//
+//  Created by CheerChen on 14-12-16.
+//  Copyright (c) 2014年 ustcsse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DriftBottles/DriftBottles/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DriftBottles
+//
+//  Created by CheerChen on 14-12-16.
+//  Copyright (c) 2014年 ustcsse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
